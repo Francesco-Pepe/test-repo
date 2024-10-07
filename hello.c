7a9139746a72e7e0f4665cf67a32d680c439d99d
Francesco Pepe <pepefrancesco960@gmail.com>
2024-10-07 17:44:36 +0200

    g

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+	int x=0;
+
+	printf("The starting value of x is %d\n", x);
+	x++;
+	printf("The next value of x is %d\n", x);
+	printf("\n\nHELLOOOOO!\nBye\n");
+	return 0;
+}
